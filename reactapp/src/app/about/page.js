36b81586e2c94ff171b879df320e2aaecbc02b28
Amaug <amaugofaith@gmail.com>
2024-05-14 13:01:56 +0100

    PUSH

---
+++ reactapp/src/app/about/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const page = () => {
+  return (
+    <>
+     <div>about page</div>
+     <Header/>
+    </>
+   
+  )
+}
+
+export default page
---
+++ reactapp/src/app/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import mylogo from '../components/Crystal-Logo 1.png'
+import Image from 'next/image'
+const Header = () => {
+  return (
+<>
+<header/>
+<Image src ={mylogo} alt="Logo" />
+<header/>
+</>
+  )
+}
+
+export default Header

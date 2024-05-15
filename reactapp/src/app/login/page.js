d4854993b6ea6321ee761da7c1fb962073cb6787
Amaug <amaugofaith@gmail.com>
2024-05-15 10:42:14 +0100

    push

---
+++ reactapp/src/app/login/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export const login = () => {
+  return (
+    <Layout>
+    <div className='center'>
+      <div className='login'>Log In</div>
+      <div className='txt'>Your personal details will help Crystal Finance know and contact you easily.</div>
+    </div>
+   </Layout>
+  )
+}
--- reactapp/src/app/page.js
+++ reactapp/src/app/page.js
@@ -1,95 +1,38 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
-export default function Home() {
+'use client'
+import React from 'react'
+import Layout from './components/Layout'
+import { useEffect, useState } from 'react';
+export const login = () => {
+  const [email, setemail] = useState("");
+  const[password, setpassword] = useState("")
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by gdgdgd xvgxxgfx fxfxf editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Layout>
+    <div className='center'>
+      <div className='login'>Log In</div>
+      <div className='txt'>Your personal details will help Crystal Finance know and contact you easily.</div>
+      <form>
+        <label>Email Address</label>
+        <input
+        type='text'
+    placeholder='Email Address'
+      value={email} // zxab
+      onChange={e => setemail(e.target.value)} // ... and update the state variable on any edits!
+    />
+     <label>Password</label>
+        <input
+        type='password'
+    placeholder='*********'
+      value={password} // zxab
+      onChange={e => setpassword(e.target.value)} // ... and update the state variable on any edits!
+    />
+        
+      </form>
+      <div className='forgot'>Forgot Password?</div>
+      <button class="custom-button">Log In</button>
+    </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+   </Layout>
+  )
 }
+
+export default login
